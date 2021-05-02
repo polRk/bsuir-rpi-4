8a06d4acbf771f0a1765f463c7b23c07b97b7861
Vladislav Polyakov <polrkov.tm@gmail.com>
2021-05-02 17:59:23 +0300

    chore: setup project

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  mode: 'jit',
+  purge: [
+    './src/**/*.html',
+    './src/**/*.ts',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
+}
