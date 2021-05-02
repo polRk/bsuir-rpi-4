b1c23571425fe0598f025da87fb0155208d071db
Vladislav Polyakov <polrkov.tm@gmail.com>
2021-05-02 17:59:40 +0300

    feat: add utils

---
+++ src/utils/debounce.ts
@@ -0,0 +1,17 @@
+export function debounce<T extends (...args: any[]) => any>(
+  callback: T,
+  ms: number,
+): (...args: Parameters<T>) => Promise<ReturnType<T>> {
+  let timer: ReturnType<typeof setTimeout>
+
+  return (...args: Parameters<T>) => {
+    if (timer) clearTimeout(timer)
+
+    return new Promise<ReturnType<T>>((resolve) => {
+      timer = setTimeout(() => {
+        const returnValue = callback(...args) as ReturnType<T>
+        resolve(returnValue)
+      }, ms)
+    })
+  }
+}
